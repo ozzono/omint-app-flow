65e4ccc963177853417e317f256e4de01c4970a7
Hugo Virgilio <huvirgilio@gmail.com>
2020-11-02 23:16:59 -0300

    Added parsePDF method

--- omint-app-flow.go
+++ omint-app-flow.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"code.sajari.com/docconv"
 	adb "github.com/ozzono/adbtools"
 )
 
@@ -79,14 +80,17 @@ func main() {
 	}
 	defer flow.close()
 
-	flow.device.WakeUp()
-	flow.device.Swipe([4]int{int(flow.device.Screen.Width / 2), flow.device.Screen.Height - 100, int(flow.device.Screen.Width / 2), 100})
-	_, err = flow.OmintInvoice()
+	if !strings.Contains(flow.device.ID, "emulator") {
+		flow.device.WakeUp()
+		flow.device.Swipe([4]int{int(flow.device.Screen.Width / 2), flow.device.Screen.Height - 100, int(flow.device.Screen.Width / 2), 100})
+	}
+
+	invoice, err := flow.OmintInvoice()
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
+	fmt.Printf("invoice %#v\n", invoice)
 }
 
 func (flow *flow) OmintInvoice() (Invoice, error) {
@@ -133,63 +137,111 @@ func (flow *flow) OmintInvoice() (Invoice, error) {
 
 	flow.sleep(20)
 
-	err = flow.invoicePDF()
+	nonStop, err := flow.invoicePDF()
 	if err != nil {
 		return Invoice{}, err
 	}
 
-	return Invoice{}, nil
+	if !nonStop {
+		return Invoice{}, fmt.Errorf("No pending invoice available")
+	}
+
+	invoice, err := pdfFlow()
+	if err != nil {
+		return Invoice{}, err
+	}
+
+	return invoice, nil
 }
 
-func (flow *flow) invoicePDF() error {
+func pdfFlow() (Invoice, error) {
+	res, err := docconv.ConvertPath("invoice.pdf")
+	if err != nil {
+		return Invoice{}, fmt.Errorf("docconv.ConvertPath")
+	}
+
+	pdfText := fmt.Sprintf("%v", res)
+	pdfText = strings.Replace(pdfText, "\n\n", "\n", -1)
+	pdfText = strings.Replace(pdfText, "VALOR\n", "VALOR ", -1)
+	pdfText = strings.Replace(pdfText, "VENCIMENTO\n", "VENCIMENTO ", 1)
+	pdfText = strings.Replace(pdfText, "SANTANDER\n", "SANTANDER ", -1)
+
+	err = os.Remove("invoice.pdf")
+	if err != nil {
+		log.Printf("failed to remove invoice.pdf file; this must be verified")
+	}
+
+	return Invoice{
+		Value:   applyRegexp(expressions["pdf-value"], pdfText)[1],
+		DueDate: applyRegexp(expressions["pdf-duedate"], pdfText)[1],
+		BarCode: barCode(applyRegexp(expressions["pdf-barcode"], pdfText)[1]),
+		Status:  "pending",
+	}, nil
+}
+
+func barCode(input string) string {
+	input = strings.Replace(input, " ", "", -1)
+	input = strings.Replace(input, ".", "", -1)
+	return input
+}
+
+func (flow *flow) invoicePDF() (bool, error) {
 	log.Println("Starting invoice PDF flow")
 
 	err := flow.exp2tap(expressions["menu-btn"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	flow.sleep(10)
 
 	err = flow.exp2tap(expressions["invoice-btn"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	flow.sleep(10)
+	err = flow.device.WaitInScreen(5, "vencimento", "pagamento")
+	if err != nil {
+		return false, err
+	}
+	if !flow.device.HasInScreen(true, "aberto") {
+		return false, nil
+	}
 
 	err = flow.exp2tap(expressions["invoice-pdf"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	err = device.WaitInScreen(10, "fatura", "pdf")
 	if err != nil {
-		return err
+		return false, err
 	}
-
 	err = device.WaitInScreen(10, "download", "ok", "cancel")
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	err = flow.exp2tap(expressions["ok-btn"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	err = flow.exp2tap(expressions["more-options"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	dump := strings.Replace(strings.TrimPrefix(flow.device.Shell("adb shell uiautomator dump"), "UI hierchary dumped to: "), "\n", "", -1)
-	download := fmt.Sprintf("/%s/Download", strings.Split(string(dump[1:]), "/")[0])
+	path, err := flow.storagePath()
+	if err != nil {
+		return false, err
+	}
+	download := fmt.Sprintf("%s/Download", path)
 	flow.device.Shell(fmt.Sprintf("adb shell rm %s/*", download))
 
 	err = flow.exp2tap(expressions["dl-button"])
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	if err := flow.device.WaitInScreen(2, "allow_button", "deny", "permission_message"); err != nil {
@@ -197,7 +249,7 @@ func (flow *flow) invoicePDF() error {
 	} else {
 		err = flow.exp2tap(expressions["allow-button"])
 		if err != nil {
-			return err
+			return false, err
 		}
 	}
 
@@ -205,15 +257,15 @@ func (flow *flow) invoicePDF() error {
 		"(Fatura_\\d{7}.pdf)", //Fatura_0000000.pdf
 		flow.device.Shell("adb shell ls "+download))[1]
 	if len(file) == 0 {
-		return fmt.Errorf("failed to find invoice pdf file")
+		return false, fmt.Errorf("failed to find invoice pdf file")
 	}
 	if !strings.Contains(flow.device.Shell(fmt.Sprintf("adb pull %s/%s invoice.pdf", download, file)), "1 file pulled") {
-		return fmt.Errorf("failed to pull %s/%s", download, file)
+		return false, fmt.Errorf("failed to pull %s/%s", download, file)
 	}
 
 	flow.device.Shell(fmt.Sprintf("adb shell rm %s/*", download))
 
-	return nil
+	return true, nil
 }
 
 func (flow *flow) loginFlow() error {
@@ -292,6 +344,9 @@ func allExpressions() map[string]string {
 		"more-options": "More options.*?(\\[\\d+,\\d+\\]\\[\\d+,\\d+\\])",
 		"allow-button": "allow_button.*?(\\[\\d+,\\d+\\]\\[\\d+,\\d+\\])",
 		"dl-button":    "Download.*?(\\[\\d+,\\d+\\]\\[\\d+,\\d+\\])",
+		"pdf-value":    "VALOR.(\\d+\\.\\d{1,3},\\d{1,2})",
+		"pdf-duedate":  "VENCIMENTO.(\\d{2}/\\d{2}/20\\d{2})",
+		"pdf-barcode":  "SANTANDER.(\\d{5}\\.\\d{5}.\\d{5}\\.\\d{6}.\\d{5}\\.\\d{6}.\\d.\\d{14})",
 	}
 }
 
@@ -380,4 +435,15 @@ func (flow *flow) exp2tap(exp string) error {
 	}
 	flow.device.TapScreen(coords[0], coords[1], 10)
 	return nil
+}
+
+func (flow *flow) storagePath() (string, error) {
+	dumpOutput := flow.device.Shell("adb shell uiautomator dump")
+	if !strings.Contains(dumpOutput, "dump.xml") {
+		return "", fmt.Errorf("Failed to dump screen xml: %s", dumpOutput)
+	}
+	dumpOutput = strings.Replace(dumpOutput, "\n", "", -1)
+	dumpOutput = strings.TrimPrefix(dumpOutput, "UI hierchary dumped to: ")
+	dumpOutput = strings.TrimSuffix(dumpOutput, "/window_dump.xml")
+	return dumpOutput, nil
 }
